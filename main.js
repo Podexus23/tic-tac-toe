d2464cd1c23def31546dddd03855e680cc5edaaf
Podexus23 <santon23@gmail.com>
2022-01-23 01:17:51 +0300

    feat: add listener for cell

--- main.js
+++ main.js
@@ -12,35 +12,53 @@ let GameBoard = (function () {
     return _gameBoard
   }
 
+  function setXY(player, coords) {
+    let [x, y] = coords.split(':')
+    _gameBoard[x][y] = player;
+  }
+
   return {
-    showGameBoard
+    showGameBoard,
+    setXY
   }
 })();
 
-
-
 let displayController = (function () {
   let playground = document.querySelector('.playground');
+  //taken from other modules
+  let gameBoard = GameBoard.showGameBoard()
+  const setCoords = GameBoard.setXY;
 
   function _createCell(content, coordinate) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     cell.textContent = content;
     cell.dataset.coord = coordinate;
+
+    cell.addEventListener('click', _cellOnClick)
     return cell
   }
-  /**
-   * Make an array of cells
-   * @returns {[Nodes]} cells
-   */
+
+  function _cellOnClick(e) {
+    console.log(e.target.dataset.coord)
+    setCoords("Y", e.target.dataset.coord)
+    changeCell("Y", e.target.dataset.coord)
+  }
+
+  function changeCell(newText, coord) {
+    let cell = document.querySelector(`.cell[data-coord='${coord}']`)
+    cell.textContent = newText;
+  }
+
   function _makeCellsFromGameBoard() {
-    let gameBoard = GameBoard.showGameBoard()
-    let packOfCells = gameBoard.map((row, x) => {
-      let cellRow = row.map((col, y) => {
+    let packOfCells = gameBoard.
+    map((row, x) => {
+      let cellRow = row.
+      map((col, y) => {
         return _createCell(col, `${x}:${y}`)
       })
       return cellRow
-    }).flat()
+    }).flat();
     return packOfCells
   }
 
@@ -53,7 +71,18 @@ let displayController = (function () {
 
   function _render() {
     _addCellsInPlayground();
-
   }
+
   _render()
 })()
+
+let createPlayer = (side, type, level) => {
+  return {
+    side,
+    type
+  }
+}
+
+let firstPlayer = createPlayer('X', 'human')
+
+console.log(firstPlayer)
