85bc91463265d1c9a27e2f27e0c7c321c7e509d1
Podexus23 <santon23@gmail.com>
2022-01-27 03:41:22 +0300

    feat: logic works, restart works

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 
 let GameBoard = (function () {
   let _gameBoard = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
   ];
 
   function showGameBoard() {
@@ -18,18 +18,40 @@ let GameBoard = (function () {
   }
 
   function checkWinPositions(side) {
-    const firstPosition = [
-      ['X', 'X', 'X'],
-      ['O', 'O', 'O'],
-      ['', '', ''],
-    ];
-    if (_gameBoard == firstPosition) console.log('firstPosistion')
-    else console.log('nope')
+    let flatBoard = _gameBoard.flat();
+    let checker;
+    //horizontal lines winner
+    _gameBoard.forEach((line) => {
+      if (line.every(elem => elem == side)) checker = true;
+    })
+    if (checker) return true;
+    //vertical lines
+    for (let i = 0; i < flatBoard.length; i++) {
+      if (flatBoard[i] == flatBoard[i + 3] && flatBoard[i] == flatBoard[i + 6] && flatBoard[i] == side) {
+        return true
+      }
+    }
+    if (flatBoard[0] == flatBoard[4] && flatBoard[0] == flatBoard[8] && flatBoard[0] == side) {
+      return true
+    }
+    if (flatBoard[2] == flatBoard[4] && flatBoard[2] == flatBoard[6] && flatBoard[2] == side) {
+      return true
+    }
+    if ((flatBoard.filter(e => e != undefined).length == 9)) return 'draw';
+  }
+
+  function resetGameBoard() {
+    _gameBoard = [
+      [undefined, undefined, undefined],
+      [undefined, undefined, undefined],
+      [undefined, undefined, undefined],
+    ]
   }
   return {
     showGameBoard,
     setXY,
-    checkWinPositions
+    checkWinPositions,
+    resetGameBoard
   }
 })();
 
@@ -47,12 +69,23 @@ let displayController = (function () {
     return cell
   }
 
+  function blockAllCells() {
+    let cells = playground.querySelectorAll('.cell')
+    cells.forEach(cell => {
+      if (cell.classList.contains('full')) return;
+      else cell.classList.add('full')
+    })
+  }
 
-  function _ChangeCellOnClick(e) {
-    if (e.target.classList.contains('cell')) {
-      console.log(e.target.dataset.coord)
-      console.log(side)
-    } else return;
+  function reset() {
+    let cells = playground.querySelectorAll('.cell')
+    cells.forEach(cell => {
+      if (cell.classList.contains('full')) {
+        cell.classList.remove('full')
+        cell.textContent = '';
+      }
+    })
+    GameBoard.resetGameBoard()
   }
 
   function changeCell(side, coord) {
@@ -88,7 +121,9 @@ let displayController = (function () {
   _render()
 
   return {
-    changeCell
+    changeCell,
+    blockAllCells,
+    reset
   }
 })()
 
@@ -97,6 +132,7 @@ let playerController = (function () {
   let firstPlayer, secondPlayer;
   let mainKey = 'first';
   const header = document.querySelector('header');
+  let startSide = "X";
 
   function createChooseMenu(number) {
     let chooseDiv = document.createElement('div');
@@ -142,7 +178,6 @@ let playerController = (function () {
       mainKey = 'second';
     } else if (key == 'second') {
       const playerChoose = document.querySelector('.player-choose')
-      console.log(key)
       playerChoose.remove()
       mainKey = 'end';
     }
@@ -178,7 +213,6 @@ let playerController = (function () {
     if (choices.length == 2 || mainKey == 'second') {
       if (mainKey == 'first') firstPlayer = createPlayer(choices[0].textContent.toUpperCase(), choices[1].textContent);
       if (mainKey == 'second') secondPlayer = createPlayer(firstPlayer.side == 'X' ? "O" : 'X', choices[0].textContent);
-      console.log(firstPlayer, secondPlayer)
       removeChooseMenu(mainKey)
     } else console.log('you must make your choice')
   }
@@ -188,39 +222,69 @@ let playerController = (function () {
       choosePlayerOptions(key)
     }
     if (key == 'second') {
-      console.log('hi')
       choosePlayerOptions(key)
     }
     if (key == 'end') {
-      console.log(`let's play`)
+
       let starter = document.createElement('h3')
+      starter.classList.add('announcer')
       starter.textContent = `Let's Begin Player One is ${firstPlayer.side} - Player Two is ${secondPlayer.side}`
       header.append(starter)
+
+      let restarter = document.createElement('button');
+      restarter.textContent = 'restart';
+      restarter.classList.add('restart-button');
+      restarter.addEventListener('click', restartTheGame)
+      header.append(restarter)
+
       playTheGame()
     }
   }
 
+  function restartTheGame() {
+    const playground = document.querySelector('.playground');
+    firstPlayer = undefined;
+    secondPlayer = undefined;
+    mainKey = 'first';
+    startSide = "X"
+    header.innerHTML = `<h1 class="header-title">Tic Tac Toe</h1>`;
+    displayController.reset()
+    playground.removeEventListener('click', playerMove)
+    _runTheGame(mainKey)
+  }
+
+  function showTheWinner(side) {
+    let announcer = header.querySelector('.announcer')
+    if (firstPlayer.side == side) {
+      announcer.textContent = `Congratulations Player One`
+    } else {
+      announcer.textContent = `Congratulations Player Two`
+    }
+    if (side == 'draw') announcer.textContent = `It's a draw`;
+  }
+
+  function sideChanger(side) {
+    return (side == "X") ? "O" : "X"
+  }
+
   function playTheGame() {
     const playground = document.querySelector('.playground');
-    const changeCell = displayController.changeCell;
-    let startSide = "X";
-    let winner;
-
-
     playground.addEventListener('click', playerMove)
+  }
 
-    function sideChanger(side) {
-      return (side == "X") ? "O" : "X"
-    }
-
-    function playerMove(event) {
-      if (!event.target.classList.contains('cell')) {
-        return
-      } else if (event.target.classList.contains('full')) {
-        return
+  function playerMove(event) {
+    if (!event.target.classList.contains('cell')) {
+      return;
+    } else if (event.target.classList.contains('full')) {
+      return;
+    } else {
+      displayController.changeCell(startSide, event.target.dataset.coord)
+      if (GameBoard.checkWinPositions(startSide) == 'draw') {
+        showTheWinner('draw')
+      } else if (GameBoard.checkWinPositions(startSide) === true) {
+        displayController.blockAllCells();
+        showTheWinner(startSide);
       } else {
-        changeCell(startSide, event.target.dataset.coord)
-        GameBoard.checkWinPositions(startSide)
         startSide = sideChanger(startSide);
       }
     }
